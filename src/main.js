63c614255da6c282bebe53a9094ba67bc0796f63
John Lynch <carter@tiogaventures.com>
2022-04-18 16:34:19 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// Points Breakdown: 80
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
 let config = {
     type: Phaser.CANVAS,
     width: 640,
